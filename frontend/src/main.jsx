e9ab7f781653dac4d723d99609cd4561fbb353f8
TechNinjaAyush <ayushkhairnar06@gmail.com>
2026-07-22 17:09:10 +0530

    ahbajf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' 
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
 console.log("CLIENT_ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
 console.log("MAPS_KEY:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
-
+console.log("ALL ENV:", import.meta.env);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+
+
